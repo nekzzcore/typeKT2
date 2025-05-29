1cfb62ab6557e6ebad507765ba83bdd7ce4cacff
nekzzcore <nekzzcore@gmail.com>
2025-05-29 17:50:22 +1000

    Add files via upload

--- script.ts
+++ script.ts
@@ -61,9 +61,9 @@ class Book extends Publisher implements Reception {
         if (this.getCopies() > 0 && reader.items.length < 5) {
             this.setCopies(this.getCopies() - 1);
             reader.items.push(this);
-            console.log(Книга "${this.getTitle()}" выдана читателю ${reader.firstName} ${reader.lastName});
+            console.log(`${this.getTitle()} ${reader.firstName} ${reader.lastName}`);
         } else {
-            console.log(Книга "${this.getTitle()}" не может быть выдана.);
+            console.log(Книга `${this.getTitle()}`);
         }
     }
 
@@ -72,14 +72,14 @@ class Book extends Publisher implements Reception {
         if (index > -1) {
             this.setCopies(this.getCopies() + 1);
             reader.items.splice(index, 1);
-            console.log(Книга "${this.getTitle()}" возвращена читателем ${reader.firstName} ${reader.lastName});
+            console.log(`${this.getTitle()} ${reader.firstName} ${reader.lastName}`);
         } else {
-            console.log(Книга "${this.getTitle()}" не найдена у читателя.);
+            console.log(`${this.getTitle()}`);
         }
     }
 
     displayInfo(): void {
-        console.log(Книга: ${this.title}, Автор: ${this.author}, Год: ${this.pubYear}, Страницы: ${this.pages}, Кол-во: ${this.copies});
+        console.log(`Книга: ${this.title}, Автор: ${this.author}, Год: ${this.pubYear}, Страницы: ${this.pages}, Кол-во: ${this.copies}`);
     }
 }
 
@@ -93,3 +93,56 @@ class Magazine extends Publisher implements Reception {
     ) {
         super(title, author, pubYear, copies);
     }
+
+    
+
+    delivery(reader: Reader): void {
+        if (this.getCopies() > 0 && reader.items.length < 5) {
+            this.setCopies(this.getCopies() - 1);
+            reader.items.push(this);
+            console.log(`${this.getTitle()} ${reader.firstName} ${reader.lastName}`);
+        } else {
+            console.log(`Журнал "${this.getTitle()}" не может быть выдан`);
+        }
+    }
+
+    receive(reader: Reader): void {
+        const index = reader.items.indexOf(this);
+        if (index > -1) {
+            this.setCopies(this.getCopies() + 1);
+            reader.items.splice(index, 1);
+            console.log(`Журнал "${this.getTitle()}" возвращен читателем ${reader.firstName} ${reader.lastName}`);
+        } else {
+            console.log(`Журнал "${this.getTitle()}" не найден у читателя`);
+        }
+    }
+
+    displayInfo(): void {
+        console.log(`Журнал: ${this.title}, Автор: ${this.author}, Год: ${this.pubYear}, Номер: ${this.issue}, Кол-во: ${this.copies}`);
+    }
+}
+
+class Reader {
+    public items: Publisher[] = [];
+
+    constructor(
+        public firstName: string,
+        public lastName: string
+    ) { }
+
+    getFirstName(): string {
+        return this.firstName;
+    }
+
+    setFirstName(newFirstName: string): void {
+        this.firstName = newFirstName;
+    }
+
+    getLastName(): string {
+        return this.lastName;
+    }
+
+    setLastName(newLastName: string): void {
+        this.lastName = newLastName;
+    }
+}
